622541be326527c4b50c8ee9613eafbbfb1ee1ee
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2021-06-03 19:32:20 -0400

    added the sign up component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,6 +15,29 @@ const config = {
 
 firebase.initializeApp(config)
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return
+
+  const userRef = firestore.doc(`user/${userAuth.uid}`)
+
+  const snapShot = await userRef.get()
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch(e) {
+      console.error(`error creating user ${e.message}`)
+    }
+  }
+}
+
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
 
@@ -22,4 +45,4 @@ const provider = new firebase.auth.GoogleAuthProvider()
 provider.setCustomParameters({ prompt: 'select_account' })
 export const signInWithGoogle = () => auth.signInWithPopup(provider)
 
-export default console.log(firebase)
+export default firebase
