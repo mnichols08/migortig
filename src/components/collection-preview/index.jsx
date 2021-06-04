6f63b5377c5705a8c8ff926b8d0d3f7c8b702a28
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2021-06-04 14:01:12 -0400

    integrated with stripe for payments

--- src/components/collection-preview/index.jsx
+++ src/components/collection-preview/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 
 import CollectionItem from '../collection-item'
 
 import './styles.scss'
 
-const CollectionPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, items, history, match }) => (
     <div className="collection-preview">
-        <h1 className="title">{title.toUpperCase()}</h1>
+        <h1 
+            className="title"
+            onClick={() => history.push(`${match.path}/${title.toLowerCase()}`)}
+        >
+            {title.toUpperCase()}
+        </h1>
         <div className="preview">
             {items
             .filter((item, i) => i < 4)
@@ -15,4 +21,4 @@ const CollectionPreview = ({ title, items }) => (
     </div>
 )
 
-export default CollectionPreview
+export default withRouter(CollectionPreview)
--- src/pages/checkout/index.jsx
+++ src/pages/checkout/index.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
 import CheckoutItem from '../../components/checkout-item'
+import StripeCheckoutButton from '../../components/stripe-button'
 
 import {
     selectCartItems,
@@ -34,6 +35,16 @@ const CheckoutPage = ({ cartItems, total }) => (
             <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         ))}
         <div className="total">TOTAL: ${ total }</div>
+        <div className="test-warning">
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 
+            {parseInt(new Date().getMonth()) + 2}
+             / 
+             {parseInt(new Date().toString().slice(13,-42)) + 1}
+             - CVV: 123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 )
 
