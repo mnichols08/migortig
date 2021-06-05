5003cc65ac6462f4e4c1be9dd81d6cac7e5d4f01
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2021-06-04 22:55:00 -0400

    added media queries to styled divs

--- client/src/components/collection-item/styles.jsx
+++ client/src/components/collection-item/styles.jsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
-import CustomButton from '../custom-button/';
+import styled from 'styled-components'
+import CustomButton from '../custom-button/'
 
 export const CollectionItemContainer = styled.div`
   width: 22vw;
@@ -19,6 +19,20 @@ export const CollectionItemContainer = styled.div`
       display: flex;
     }
   }
+
+  @media screen and (max-width: 800px) {
+    width: 40vw;
+
+    &:hover {
+      .image {
+        opacity: unset;
+      }
+
+      button {
+        opacity: unset;
+      }
+    }
+  }
 `
 
 export const AddButton = styled(CustomButton)`
@@ -27,6 +41,13 @@ export const AddButton = styled(CustomButton)`
   position: absolute;
   top: 255px;
   display: none;
+
+  @media screen and (max-width: 800px) {
+    display: block;
+    opacity: 0.9;
+    min-width: unset;
+    padding: 0 10px;
+  }
 `
 
 export const BackgroundImage = styled.div`
@@ -54,5 +75,4 @@ export const NameContainer = styled.span`
 export const PriceContainer = styled.span`
   width: 10%;
   text-align: right;
-  margin-right: .75rem;
 `
